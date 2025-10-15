7dae585d3dd991a97bb2ef5efee0eb0e2b220fac
Gab Paderog <gabpaderog.dev@gmail.com>
2025-10-15 20:37:18 +0800

    add base route

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,15 @@ app.use(cors({
 app.use(express.json());
 app.use(helmet());
 
+// Base route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the HZRD API!",
+    version: "1.0.0",
+    info: "api/v1/earthquakes to get recent earthquake data",
+  });
+});
+
 // routes
 app.use('/api/v1/earthquakes', earthquakeRoutes)
 
